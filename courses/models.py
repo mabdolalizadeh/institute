d41268c7aaca1beab6fbece38b705aa10d917334
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2024-05-08 15:11:32 +0330

    changed

--- courses/models.py
+++ courses/models.py
@@ -1,12 +1,11 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
-class Course(models.Model):
-    name = models.CharField(max_length=100)
-    book = models.CharField(max_length=100)
-    level = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(3)], default=1)
-    is_active = models.BooleanField(default=True)
-    image = models.ImageField(upload_to='static/images/')
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+    price = models.IntegerField()
+
     def __str__(self):
-        return f"{self.name} - {self.book}"
+        return f"{self.title} - {self.category} - {self.level} - {self.price}"
