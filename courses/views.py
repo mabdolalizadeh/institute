d3124c27deeed609d2b4334a4650a764d73805c6
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2024-05-08 15:02:18 +0330

    changed

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'courses/index.html')
+
+
+def about(request):
+    return render(request, 'courses/about.html')
+
+
+def contact(request):
+    return render(request, 'courses/contact.html')
