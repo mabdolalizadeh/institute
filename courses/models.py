6c1e43e87a7d55630dd57e776015005b3b3cf664
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2024-05-12 23:48:15 +0330

    changed

--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
+    ages = models.CharField(max_length=100, default="All")
     level = models.CharField(max_length=100)
     price = models.IntegerField()
 
