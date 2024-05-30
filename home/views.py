5d55e51e9cfc285e7f795190871df7a71062a534
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2024-05-30 23:15:23 +0330

    home completely added

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from shop.models import Book
+from .models import Course
 
 
 def home(request):
-    return render(request, "home/home.html")
+    courses = Course.objects.all()
+    shop_elements = Book.objects.all().filter(category__title="Learning").order_by("-id")[:4]
+    return render(request, "home/home.html", {
+        "shop_elements": shop_elements,
+        "courses": courses,
+    })
--- shop/static/js/jquery.elevatezoom.js
+++ shop/static/js/jquery.elevatezoom.js
@@ -27,7 +27,7 @@
 
 if ( typeof Object.create !== 'function' ) {
 	Object.create = function( obj ) {
-		function F() {};
+		function F() {}
 		F.prototype = obj;
 		return new F();
 	};
